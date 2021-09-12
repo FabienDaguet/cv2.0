99ab166fe0b21850cafbf935134cf1f145632d79
Fabien Daguet <daguet77@gmail.com>
2021-09-12 17:16:09 +0200

    ajout d'une couleur clair pour les sous-titres, ajout de progress bar avec + js sur about me, ajout de before et after sur les titres pour style

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ let homePage = document.querySelector(".home");
 let start = document.querySelector(".start-js");
 let layout = document.querySelector(".layout-js");
 let myPage = document.querySelector("#head-js");
+let comp = document.querySelector(".comp-js");
+const progress = document.querySelectorAll(".progress-js");
 
 function welcome(e) {
     e.preventDefault(e);
 
 }
+
+function scrollAnimation() {
+
+    for (let element of progress) {
+            
+        if (comp.getBoundingClientRect().top < 596.5625) {
+            element.style.width = element.getAttribute("data-done") + "%";
+            element.style.opacity = 1;
+        } 
+    }
+}
+
+
+
+window.addEventListener("scroll", scrollAnimation); 
 
 start.addEventListener("click", welcome);
